8a65f991a9f9456f59579a064728ae95f3695957
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2023-08-16 11:53:12 -0300

    First commit

---
+++ app/src/main/java/com/example/itau_kt/PublicActivity.kt
@@ -0,0 +1,11 @@
+package com.example.itau_kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_public)
+    }
+}
