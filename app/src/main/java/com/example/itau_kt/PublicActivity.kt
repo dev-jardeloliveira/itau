b245e8a4e579eee4940a9ee4f8b6b9c2049b1bcd
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2023-08-22 10:57:56 -0300

    First commit

--- app/src/main/java/com/example/itau_kt/PublicActivity.kt
+++ app/src/main/java/com/example/itau_kt/PublicActivity.kt
@@ -1,11 +1,32 @@
 package com.example.itau_kt
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.updateLayoutParams
+import com.example.itau_kt.databinding.ActivityPublicBinding
 
 class PublicActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityPublicBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_public)
+
+        binding = ActivityPublicBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
+        binding.arrow.setOnClickListener{
+           if(binding.barTool.appBarLayout.top < 0){
+                binding.barTool.appBarLayout.setExpanded(true, true)
+               binding.arrow.setImageResource(R.drawable.ic_arrow_up)
+
+            }else{
+                binding.barTool.appBarLayout.setExpanded(false, true)
+               binding.arrow.setImageResource(R.drawable.ic_arrow_collapse)
+            }
+        }
+
     }
 }
