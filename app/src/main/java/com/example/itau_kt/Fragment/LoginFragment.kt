5f377c901eb667b2f2cf267c50cbf57738edbd37
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2023-09-27 11:20:39 -0300

    First commit

--- app/src/main/java/com/example/itau_kt/Fragment/LoginFragment.kt
+++ app/src/main/java/com/example/itau_kt/Fragment/LoginFragment.kt
@@ -1,34 +1,27 @@
 package com.example.itau_kt.Fragment
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import com.example.itau_kt.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 
-class PixFragment : Fragment() {
+class LoginFragment : Fragment() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_pix, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
 
-    }
 }
--- app/src/main/java/com/example/itau_kt/PublicActivity.kt
+++ app/src/main/java/com/example/itau_kt/PublicActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.itau_kt.Model.KeyboardPassword
 import com.example.itau_kt.ViewModel.publicViewModel
 import com.example.itau_kt.databinding.ActivityPublicBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PublicActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     val  model: publicViewModel by viewModels()
     lateinit var binding: ActivityPublicBinding
@@ -20,11 +26,15 @@ class PublicActivity : AppCompatActivity() {
         binding = ActivityPublicBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
+        var navHostController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+       navController = navHostController.navController
+        val bottoNavigatioView = findViewById<BottomNavigationView>(R.id.bntNavigation)
+        NavigationUI.setupWithNavController(bottoNavigatioView, navController)
 
-        list = model.keyboard_number()
+     //   list = model.keyboard_number()
 
 
-        binding.arrow.setOnClickListener{
+     /*   binding.arrow.setOnClickListener{
            if(binding.barTool.appBarLayout.top < 0){
                 binding.barTool.appBarLayout.setExpanded(true, true)
                binding.arrow.setImageResource(R.drawable.ic_arrow_up)
@@ -46,11 +56,11 @@ class PublicActivity : AppCompatActivity() {
         binding.keyboard2.num2.setOnClickListener {
             binding.input.text!!.append(list[1].value.toString())
         }
-
+*/
 
     }
 
-    fun btnClickAcessar(){
+  /*  fun btnClickAcessar(){
         binding.btn.textBtn.visibility =  android.view.View.GONE
         binding.btn.progressBar.visibility =  android.view.View.VISIBLE
         binding.keyboard2.keyboard.visibility = View.VISIBLE
@@ -72,7 +82,7 @@ class PublicActivity : AppCompatActivity() {
     }
     fun setInputPassword(number: Int){
         binding.input.text!!.append(number.toString())
-    }
+    }*/
 }
 
 
